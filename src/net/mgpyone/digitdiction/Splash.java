0fe64713121d485115684afaafae55fd5dae6592
mgpyone <mgpyone@mysteryzillion.com>
2013-02-15 10:19:11 +0630

    Added Splash.java for Splash Screen Implementation

---
+++ src/net/mgpyone/digitdiction/Splash.java
@@ -0,0 +1,49 @@
+package net.mgpyone.digitdiction;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Splash extends Activity {
+	@SuppressLint("HandlerLeak")
+	public class splash extends Activity {
+	       
+        private static final int STOPSPLASH = 0;
+        //time in milliseconds
+        private static final long SPLASHTIME = 3000;
+       
+        private ImageView splash;
+       
+        //handler for splash screen
+        private Handler splashHandler = new Handler() {
+                /* (non-Javadoc)
+                 * @see android.os.Handler#handleMessage(android.os.Message)
+                 */
+                @Override
+                public void handleMessage(Message msg) {
+                        switch (msg.what) {
+                        case STOPSPLASH:
+                                //remove SplashScreen from view
+                                splash.setVisibility(View.GONE);
+                                break;
+                        }
+                        super.handleMessage(msg);
+                }
+        };
+       
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.main);
+                        splash = (ImageView) findViewById(R.id.splash);
+                        Message msg = new Message();
+                        msg.what = STOPSPLASH;
+                        splashHandler.sendMessageDelayed(msg, SPLASHTIME);
+    	}
+	}
+}
